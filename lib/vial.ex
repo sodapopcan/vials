4f02f4321ab75792b48b251f4271bac15d403420
Andrew Haust <andrew@hau.st>
2023-02-16 19:05:41 -0500

    Add commented out code

--- lib/vial.ex
+++ lib/vial.ex
@@ -31,4 +31,19 @@ defmodule Vial do
       options: options
     }
   end
+
+#   def run_actions(vial) do
+#     do_run_actions(vial, vial.module.actions())
+#   end
+
+#   def do_run_actions(vial, []), do: vial
+
+#   def do_run_actions(vial, [action | actions]) do
+#     vial = run_action(vial, action)
+#     do_run_actions(vial, actions)
+#   end
+
+#   def run_action(vial, {:create_file, [filename, contents]}) do
+#     # File.write!(Path.join(vial.cwd, ))
+#   end
 end
