286950584081787d1880ff6076cd91710a982308
Andrew Haust <andrew@hau.st>
2023-02-17 14:37:49 -0500

    Add a parse function

--- lib/vial.ex
+++ lib/vial.ex
@@ -1,5 +1,44 @@
 defmodule Vial do
-  defstruct [:module, :cwd, :task, :task_args, :options, :variables]
+  defstruct [:module_location, :module, :cwd, :task, :task_args, :options, :args, :variables]
+
+  def parse(args) do
+    {vial_options, rest} =
+      OptionParser.parse_head!(args,
+        switches: [location: :string],
+        aliases: [l: :location]
+      )
+
+    {options, task_data, _} =
+      OptionParser.parse(rest,
+        switches: [],
+        allow_nonexistent_atoms: true
+      )
+
+    [task | task_args] = task_data
+
+    module_location =
+      if vial_options[:location] do
+        vial_options[:location]
+      else
+        if Mix.env() == :test, do: "tmp", else: "tmp"
+      end
+
+    args =
+      task_args
+      |> Enum.with_index()
+      |> Enum.map(fn {arg, index} -> {:"#{index + 1}", arg} end)
+      |> Enum.into(%{})
+      |> Map.merge(Enum.into(options, %{}))
+
+    %Vial{
+      module_location: module_location,
+      cwd: File.cwd!(),
+      task: task,
+      task_args: task_args,
+      options: options,
+      args: args
+    }
+  end
 
   def load(args) do
     {vial_options, rest} =
--- test/vial_test.exs
+++ test/vial_test.exs
@@ -3,6 +3,31 @@ defmodule VialTest do
 
   @subject Vial
 
+  describe "parse/1" do
+    @tag :tmp_dir
+    test "parses mix args and returns a %Vial{} struct", %{tmp_dir: tmp_dir} do
+      path = Path.join(tmp_dir, "mod.one.ex")
+
+      vial =
+        @subject.parse(
+          ~w[-l #{tmp_dir} mod.one some-arg some-other-arg --binary-id --database sqlite]
+        )
+
+      assert vial.module_location == tmp_dir
+      assert vial.cwd == File.cwd!()
+      assert vial.task == "mod.one"
+      assert vial.task_args == ["some-arg", "some-other-arg"]
+      assert vial.options == [binary_id: true, database: "sqlite"]
+
+      assert vial.args == %{
+               :"1" => "some-arg",
+               :"2" => "some-other-arg",
+               binary_id: true,
+               database: "sqlite"
+             }
+    end
+  end
+
   describe "load/1" do
     @tag :tmp_dir
     test "loads vial module from args and returns a %Vial{} struct", %{tmp_dir: tmp_dir} do
