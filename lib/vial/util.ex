28e7aa9a881c2cd6c5c647d3b986d5bf8c3d710f
Andrew Haust <andrew@hau.st>
2023-02-19 17:41:38 -0500

    Return unwrapped ast

--- lib/vial/util.ex
+++ lib/vial/util.ex
@@ -7,12 +7,15 @@ defmodule Vial.Util do
   end
 
   def inject_into_module_body(ast, quoted) do
-    Macro.prewalk(ast, false, fn
-      [do: block], false ->
-        {[do: [quoted | [block]]], true}
+    {ast, _} =
+      Macro.prewalk(ast, false, fn
+        [do: block], false ->
+          {[do: [quoted | List.wrap(block)]], true}
 
-      other, acc ->
-        {other, acc}
-    end)
+        other, acc ->
+          {other, acc}
+      end)
+
+    ast
   end
 end
