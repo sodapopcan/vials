5f21fa189a840f35278278d855e36f54c819b9d4
Andrew Haust <andrew@hau.st>
2023-02-27 18:43:36 -0500

    Add DSL functions to the formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,15 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  export: [
+    locals_without_parens: [
+      create: 2,
+      edit: 2,
+      remove: 1,
+      base_path: 1,
+      add_dep: 1,
+      remove_comments: 0,
+      remove_comments: 1
+    ]
+  ]
 ]
