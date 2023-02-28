810b7ef06354f5c05303a15ff8f5c82b2cf7288a
Andrew Haust <andrew@hau.st>
2023-02-28 13:31:36 -0500

    That was a fun exercise but just use Sourceror

--- lib/vials/actions.ex
+++ lib/vials/actions.ex
@@ -6,19 +6,12 @@ defmodule Vials.Actions do
   @comment ~r/\A(\s+)?#/
 
   def remove_comments(string) do
-    string
-    |> String.split("\n")
-    |> Enum.reduce({[], false}, fn line, {lines, in_doc?} ->
-      cond do
-        line =~ @doc_start -> {[line | lines], true}
-        line =~ @doc_end -> {[line | lines], false}
-        line =~ @comment and not in_doc? -> {lines, in_doc?}
-        true -> {[line | lines], in_doc?}
-      end
-    end)
-    |> elem(0)
-    |> Enum.reverse()
-    |> Enum.join("\n")
+    {ast, _comments} =
+      string
+      |> Sourceror.parse_string!()
+      |> Sourceror.Comments.extract_comments()
+
+    Sourceror.to_string(ast) <> "\n"
   end
 
   def add_dep(file_contents, dep) do
